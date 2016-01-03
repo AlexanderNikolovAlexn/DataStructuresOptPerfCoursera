1f57ddf15ba179abed75b0569f6a4227ac1982f9
Alexander Nikolov <alexander.nikolov.alexn@gmail.com>
2016-01-03 14:15:15 +0200

    added started code for week 2

--- src/document/BasicDocument.java
+++ src/document/BasicDocument.java
@@ -1,108 +1,141 @@
 package document;
 
-import java.util.List;
-
 /** 
- * A naive implementation of the Document abstract class. 
+ * A class that represents a text document
  * @author UC San Diego Intermediate Programming MOOC team
  */
-public class BasicDocument extends Document 
-{
-	/** Create a new BasicDocument object
-	 * 
-	 * @param text The full text of the Document.
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public abstract class Document {
+
+	private String text;
+	
+	/** Create a new document from the given text.
+	 * Because this class is abstract, this is used only from subclasses.
+	 * @param text The text of the document.
 	 */
-	public BasicDocument(String text)
+	protected Document(String text)
 	{
-		super(text);
+		this.text = text;
 	}
 	
-	
-	/**
-	 * Get the number of words in the document.
-	 * "Words" are defined as contiguous strings of alphabetic characters
-	 * i.e. any upper or lower case characters a-z or A-Z
-	 * 
-	 * @return The number of words in the document.
+	/** Returns the tokens that match the regex pattern from the document 
+	 * text string.
+	 * @param pattern A regular expression string specifying the 
+	 *   token pattern desired
+	 * @return A List of tokens from the document text that match the regex 
+	 *   pattern
 	 */
-	@Override
-	public int getNumWords()
+	protected List<String> getTokens(String pattern)
 	{
-		//TODO: Implement this method.  See the Module 1 support videos 
-	    // if you need help.
-		List<String> words = this.getTokens("[a-zA-Z]+");
-	    return words.size();
-	}
-	
-	/**
-	 * Get the number of sentences in the document.
-	 * Sentences are defined as contiguous strings of characters ending in an 
-	 * end of sentence punctuation (. ! or ?) or the last contiguous set of 
-	 * characters in the document, even if they don't end with a punctuation mark.
-	 * 
-	 * @return The number of sentences in the document.
-	 */
-	@Override
-	public int getNumSentences()
-	{
-	    //TODO: Implement this method.  See the Module 1 support videos 
-        // if you need help.
-		List<String> sentences = this.getTokens("[.!?]+|[^.!?]$");
-        return sentences.size();
-	}
-	
-	/**
-	 * Get the number of syllables in the document.
-	 * Words are defined as above.  Syllables are defined as:
-	 * a contiguous sequence of vowels, except for a lone "e" at the 
-	 * end of a word if the word has another set of contiguous vowels, 
-	 * makes up one syllable.   y is considered a vowel.
-	 * @return The number of syllables in the document.
-	 */
-	@Override
-	public int getNumSyllables()
-	{
-	    //TODO: Implement this method.  See the Module 1 support videos 
-        // if you need help.
-		List<String> words = this.getTokens("[a-zA-Z]+");
-		int result = 0;
-		for (String word : words) {
-			result += this.countSyllables(word);
-		}
-        return result;
-	}
-	
-	
-	/* The main method for testing this class. 
-	 * You are encouraged to add your own tests.  */
-	public static void main(String[] args)
-	{
-//		testCase(new BasicDocument("This is a test.  How many???  "
-//		        + "Senteeeeeeeeeences are here... there should be 5!  Right?"),
-//				16, 13, 5);
-//		testCase(new BasicDocument(""), 0, 0, 0);
-//		testCase(new BasicDocument("sentence, with, lots, of, commas.!  "
-//		        + "(And some poaren)).  The output is: 7.5."), 15, 11, 4);
-//		testCase(new BasicDocument("many???  Senteeeeeeeeeences are"), 6, 3, 2);
-//		testCase(new BasicDocument("Here is a series of test sentences. Your program should "
-//				+ "find 3 sentences, 33 words, and 49 syllables. Not every word will have "
-//				+ "the correct amount of syllables (example, for example), "
-//				+ "but most of them will."), 49, 33, 3);
-//		testCase(new BasicDocument("Segue"), 2, 1, 1);
-//		testCase(new BasicDocument("Sentence"), 2, 1, 1);
-//		testCase(new BasicDocument("Sentences?!"), 3, 1, 1);
-//		testCase(new BasicDocument("Lorem ipsum dolor sit amet, qui ex choro quodsi moderatius, nam dolores explicari forensibus ad."),
-//		         32, 15, 1);
+		ArrayList<String> tokens = new ArrayList<String>();
+		Pattern tokSplitter = Pattern.compile(pattern);
+		Matcher m = tokSplitter.matcher(text);
 		
-		Document doc = new BasicDocument("one (1), two (2), three (3)");
-		List<String> list = doc.getTokens("[^, ]+");
-		System.out.println(list.toString());
-		List<String> list2 = doc.getTokens("[^,]+");
-		System.out.println(list2.toString());
-		List<String> list3 = doc.getTokens("[a-z()0-9]+");
-		System.out.println(list3.toString());
-		List<String> list4 = doc.getTokens("[a-z]+|[()0-9]+");
-		System.out.println(list4.toString());
+		while (m.find()) {
+			tokens.add(m.group());
+		}
+		
+		return tokens;
 	}
+	
+	// This is a helper function that returns the number of syllables
+	// in a word.  You should write this and use it in your 
+	// BasicDocument class.
+	protected static int countSyllables(String word)
+	{
+	    //System.out.print("Counting syllables in " + word + "...");
+		int numSyllables = 0;
+		boolean newSyllable = true;
+		String vowels = "aeiouy";
+		char[] cArray = word.toCharArray();
+		for (int i = 0; i < cArray.length; i++)
+		{
+		    if (i == cArray.length-1 && Character.toLowerCase(cArray[i]) == 'e' 
+		    		&& newSyllable && numSyllables > 0) {
+                numSyllables--;
+            }
+		    if (newSyllable && vowels.indexOf(Character.toLowerCase(cArray[i])) >= 0) {
+				newSyllable = false;
+				numSyllables++;
+			}
+			else if (vowels.indexOf(Character.toLowerCase(cArray[i])) < 0) {
+				newSyllable = true;
+			}
+		}
+		//System.out.println( "found " + numSyllables);
+		return numSyllables;
+	}
+	
+	/** A method for testing
+	 * 
+	 * @param doc The Document object to test
+	 * @param syllables The expected number of syllables
+	 * @param words The expected number of words
+	 * @param sentences The expected number of sentences
+	 * @return true if the test case passed.  False otherwise.
+	 */
+	public static boolean testCase(Document doc, int syllables, int words, int sentences)
+	{
+		System.out.println("Testing text: ");
+		System.out.print(doc.getText() + "\n....");
+		boolean passed = true;
+		int syllFound = doc.getNumSyllables();
+		int wordsFound = doc.getNumWords();
+		int sentFound = doc.getNumSentences();
+		if (syllFound != syllables) {
+			System.out.println("\nIncorrect number of syllables.  Found " + syllFound 
+					+ ", expected " + syllables);
+			passed = false;
+		}
+		if (wordsFound != words) {
+			System.out.println("\nIncorrect number of words.  Found " + wordsFound 
+					+ ", expected " + words);
+			passed = false;
+		}
+		if (sentFound != sentences) {
+			System.out.println("\nIncorrect number of sentences.  Found " + sentFound 
+					+ ", expected " + sentences);
+			passed = false;
+		}
+		
+		if (passed) {
+			System.out.println("passed.\n");
+		}
+		else {
+			System.out.println("FAILED.\n");
+		}
+		return passed;
+	}
+	
+	
+	/** Return the number of words in this document */
+	public abstract int getNumWords();
+	
+	/** Return the number of sentences in this document */
+	public abstract int getNumSentences();
+	
+	/** Return the number of syllables in this document */
+	public abstract int getNumSyllables();
+	
+	/** Return the entire text of this document */
+	public String getText()
+	{
+		return this.text;
+	}
+	
+	/** return the Flesch readability score of this document */
+	public double getFleschScore()
+	{
+		double wordCount = (double)getNumWords();
+		return 206.835 - (1.015 * ((wordCount)/getNumSentences())) 
+				- (84.6 * (((double)getNumSyllables())/wordCount));
+	
+	}
+	
+	
 	
 }
